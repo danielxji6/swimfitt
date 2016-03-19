4a4840bb2d6e6ead959d784e8ccd1018fb3f7a35
danielxji6 <danielxji6@gmail.com>
2016-03-19 15:53:21 -0700

    reset to api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
 
 	def new
 		if logged_in?
-			redirect_to events_path
 		end
 		@user = User.new
 	end
@@ -12,17 +11,11 @@ class SessionsController < ApplicationController
 		@user = User.confirm(user_params)
 		if @user
 			login(@user)
-			redirect_to events_path
-	    flash[:notice] = "Successfully logged in"
-		else
-			redirect_to login_path
-			flash[:error] = "Invalid credentials. Please try again."
 		end
 	end
 
 	def destroy
 		logout
-		redirect_to root_path
 	end
 
 end
